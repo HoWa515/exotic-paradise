87532c693a128899be96fae840d7240eeba1683d
wh <sunfortea123@gmail.com>
2025-06-02 16:23:27 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable*/
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Exotic Paradise</Heading>
+            <div>
+              <Heading as="h2"> Check in/out</Heading>
+              <Button>CHECK IN</Button>
+              <Button variation="secondary" size="small">
+                CHECK OUT
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
