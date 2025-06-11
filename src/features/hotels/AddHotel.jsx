2d7a565ea53f7dd8ee7d5df624baf69f09f8d969
wh <sunfortea123@gmail.com>
2025-06-11 14:45:54 +0200

    m

--- src/features/hotels/AddHotel.jsx
+++ src/features/hotels/AddHotel.jsx
@@ -1,27 +1,46 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateHotelForm from "./CreateHotelForm";
 
 function AddHotel() {
-  const [isOpenModal, setisOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button
-        onClick={() => {
-          setisOpenModal((show) => !show);
-        }}
-      >
-        Add new hotel
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setisOpenModal(false)}>
-          <CreateHotelForm onCloseModal={() => setisOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      {/* opens prop is added for open only one window at a time  */}
+      <Modal.Open opens="hotel-form">
+        <Button>Add new hotel</Button>
+      </Modal.Open>
+      <Modal.Window name="hotel-form">
+        <CreateHotelForm />
+      </Modal.Window>
+
+      {/* <Modal.Open opens="table">
+        <Button>Add new hotel</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <HotelTable/>
+      </Modal.Window> */}
+    </Modal>
   );
 }
+// function AddHotel() {
+//   const [isOpenModal, setisOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button
+//         onClick={() => {
+//           setisOpenModal((show) => !show);
+//         }}
+//       >
+//         Add new hotel
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setisOpenModal(false)}>
+//           <CreateHotelForm onCloseModal={() => setisOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddHotel;
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,4 +1,5 @@
 /*eslint-disable*/
+import { cloneElement, useContext, useState, createContext } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -52,18 +53,49 @@ const Button = styled.button`
   }
 `;
 
-function Modal({ children, onClose }) {
+//  1) context
+const modalContext = createContext();
+
+// 2) parent component
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+  const close = () => setOpenName("");
+  const open = setOpenName;
+  return (
+    <modalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </modalContext.Provider>
+  );
+}
+
+// 3) children components
+
+// Open is the button to open Modal
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(modalContext);
+  // cloneElement: the button to open window in AddHotel.jsx
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(modalContext);
+  if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={onClose}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
 }
+
+// 4)
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
