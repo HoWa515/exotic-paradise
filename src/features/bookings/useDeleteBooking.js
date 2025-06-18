63118f365c944219fbcf910975edb816879785a7
wh <sunfortea123@gmail.com>
2025-06-18 11:50:54 +0200

    m

---
+++ src/features/bookings/useDeleteBooking.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { deleteBooking as deleteBookingApi } from "../../services/apiBookings";
+
+export function useDeleteBooking() {
+  // tanstack: Delete hotel
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate: deleteBooking } = useMutation({
+    mutationFn: (id) => deleteBookingApi(id),
+    // onSuccess:to refetch data, so UI updated
+    onSuccess: () => {
+      toast.success("Booking deleted!");
+      queryClient.invalidateQueries({
+        queryKey: ["bookings"],
+      });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { isDeleting, deleteBooking };
+}
