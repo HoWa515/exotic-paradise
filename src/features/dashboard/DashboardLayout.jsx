4d73d5aba0a64c7ba1fefc57880be91912275f82
wh <sunfortea123@gmail.com>
2025-07-01 09:48:52 +0200

    m

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,17 +1,36 @@
+/*eslint-disable*/
 import styled from "styled-components";
-
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useFetchHotels } from "../hotels/useFetchHotels";
 const StyledDashboardLayout = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
 
 function DashboardLayout() {
+  const { bookings, isLoading } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoading1,
+    numDays,
+  } = useRecentStays();
+  const { hotels, isLoading: isLoading2 } = useFetchHotels();
+  if (isLoading || isLoading1 || isLoading2) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
-      <div>activity</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        hotelCount={hotels.length}
+      />
     </StyledDashboardLayout>
   );
 }
